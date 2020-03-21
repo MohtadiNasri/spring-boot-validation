645b913871b212f9727105825e5fdfdac6ffd938
nasrimohtadi@gmail.com <Awkward@009>
2020-03-22 00:24:29 +0100

    Added Validation annotation

--- src/main/java/com/springboot/springbootvalidation/model/User.java
+++ src/main/java/com/springboot/springbootvalidation/model/User.java
@@ -1,20 +1,38 @@
 package com.springboot.springbootvalidation.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 public class User {
     @NotBlank(message = "username cannot be empty")
     private String name;
     @NotBlank(message = "password cannot be empty")
+    @Length(min=6, max=10, message="length of password should between 6 to 10 characters")
     private String password;
+    @Min(value=0)
+    @Max(value=100)
     private Double grade;
+    @Email
+    private String email;
+
+    public User() {
+    }
 
     public User(String name, String password, Double grade) {
         this.name = name;
         this.password = password;
         this.grade = grade;
     }
-
+    public User(String name, String password, Double grade, String email) {
+        this.name = name;
+        this.password = password;
+        this.grade = grade;
+        this.email = email;
+    }
     public String getName() {
         return name;
     }
@@ -39,12 +57,21 @@ public class User {
         this.grade = grade;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
                 ", password='" + password + '\'' +
                 ", grade=" + grade +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
