53b01f358d2447c0501bc9553b48024fc2201f4c
nasrimohtadi@gmail.com <Awkward@009>
2020-03-22 00:14:07 +0100

    Added som comments to clarify code

--- src/main/java/com/springboot/springbootvalidation/controller/UserController.java
+++ src/main/java/com/springboot/springbootvalidation/controller/UserController.java
@@ -12,12 +12,14 @@ public class UserController {
 
     @RequestMapping("add")
     public String toAdd(User user) {
+        //instead of writing add.html
         return "add";
     }
 
     @RequestMapping("addUser")
     public String add(@Valid User user, BindingResult result) {
         if (result.hasErrors()) {
+            //if you are miss-filled any input field you will redirected to the same page with errors !
             return "add";
         }
         System.out.println("Save user =" + user.toString());
